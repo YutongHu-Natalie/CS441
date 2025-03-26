59b6ff88cab89954e913130a6e3a274f44745328
YutongHu-Natalie <yutong.hu@emory.edu>
2025-03-25 23:27:06 -0400

    Page issues

--- website/boxplot.js
+++ website/boxplot.js
@@ -1,9 +1,10 @@
 // Encapsulate everything in an IIFE to avoid global namespace pollution
 (function() {
     const basePath = window.location.hostname === "localhost" || 
-                window.location.hostname === "127.0.0.1" 
-                ? "" 
-                : "/CS441";
+                window.location.hostname === "127.0.0.1" 
+                ? "" 
+                : "/Final_Project";
+
     // Define chart dimensions
     const margin = { top: 50, right: 40, bottom: 70, left: 200 };
     const width = 700, height = 400;
